30ff91c12ee358240015ccc134c2b693a80e6c14
ujjawalontop2 <ujjawalojha@gmail.com>
2023-02-24 10:55:14 +0530

    toolbox

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='toolbox',
-    version='0.1.3',
+    version='0.1.4',
     author='Ujjawal',
     author_email='ujjawalojha@gmail.com',
     description='custom logging file',
