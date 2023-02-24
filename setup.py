e331c9806eeeed2ae1ebe9bfa172dff8d7106931
ujjawalontop2 <ujjawalojha@gmail.com>
2023-02-24 10:49:30 +0530

    new method 2

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
-    version='0.0.3',
+    name='toolbox_new',
+    version='0.1.2',
     author='Ujjawal',
     author_email='ujjawalojha@gmail.com',
     description='custom logging file',
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/ujjawalontop2/toolbox',
     license='MIT',
-    packages=['toolbox'],
+    packages=['toolbox_new'],
     install_requires=['Python==3.8.10'],
     zip_safe=False
 )
